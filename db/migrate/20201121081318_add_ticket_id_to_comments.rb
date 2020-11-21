a9f92eb44a90df66385c07732e2ab83133f43226
Lushen2015 <lushen2015@gmail.com>
2020-11-21 10:31:45 +0200

    Initial adding of comments

---
+++ db/migrate/20201121081318_add_ticket_id_to_comments.rb
@@ -0,0 +1,5 @@
+class AddTicketIdToComments < ActiveRecord::Migration[6.0]
+  def change
+    add_column :comments, :ticket_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_192059) do
+ActiveRecord::Schema.define(version: 2020_11_21_081318) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tickets", force: :cascade do |t|
     t.string "student_id"
